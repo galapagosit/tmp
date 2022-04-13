d4830bf5ab656ba27ec1ffe0b9ebbc64b04c35ac
Daisuke Hashimoto <galapagosit@gmail.com>
2022-04-14 01:46:44 +0900

    init

--- okamoto-amon.zsh
+++ okamoto-amon.zsh
@@ -1,7 +1,7 @@
-osascript -e "set volume 96/100*7"
 say 'ごめんなさいめんどくさすぎました'
 say 'ググレカス'
 say 'ググレカス'
 say 'ググレカス'
 
+osascript -e "set volume 96/100*7"
 open 'https://www.youtube.com/watch?v=EVdGzKMBiIE'
